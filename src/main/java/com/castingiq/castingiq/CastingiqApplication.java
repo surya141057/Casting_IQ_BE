7b8eb65776d4b5f064d019c0a8eb40bf15383a5b
surya141057 <suryakrishnamurthy14@gmail.com>
2025-02-07 21:28:08 +0530

    new

---
+++ src/main/java/com/castingiq/castingiq/CastingiqApplication.java
@@ -0,0 +1,13 @@
+package com.castingiq.castingiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastingiqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastingiqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/castingiq/castingiq/CastingiqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castingiq.castingiq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastingiqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
